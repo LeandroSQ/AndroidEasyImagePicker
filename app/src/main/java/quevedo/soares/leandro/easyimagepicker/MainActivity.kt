44a9f3d37af7deefc00f58d0c53ad33adc5af505
Leandro Soares Quevedo <leandro.soares@operacao.rcadigital.com.br>
2019-07-09 14:24:49 -0300

    Initialized native cropping feature

--- app/src/main/java/quevedo/soares/leandro/easyimagepicker/MainActivity.kt
+++ app/src/main/java/quevedo/soares/leandro/easyimagepicker/MainActivity.kt
@@ -1,60 +1,68 @@
 package quevedo.soares.leandro.easyimagepicker
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import quevedo.soares.leandro.lib.CropImageDialog
 import quevedo.soares.leandro.lib.EasyImagePicker
 
 class MainActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
-	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-		super.onActivityResult(requestCode, resultCode, data)
-	}
 
-	fun onClickCamera(v: View) {
-		EasyImagePicker.with(this)
-				.crop()
-				.listener { error, bitmap ->
-					if (error != null) {
-						Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
-					} else {
-						image.setImageBitmap(bitmap)
-					}
-				}
-				.pick(EasyImagePicker.ImageSource.CAMERA)
+    fun onClickCamera(v: View) {
+        CropImageDialog.show(this, BitmapFactory.decodeResource(resources, R.drawable.credit_card))
+        return
 
-	}
+        EasyImagePicker.with(this)
+            .crop()
+            .listener { error, bitmap ->
+                if (error != null) {
+                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    image.setImageBitmap(bitmap)
+                }
+            }
+            .pick(EasyImagePicker.ImageSource.CAMERA)
 
-	fun onClickGallery(v: View) {
-		EasyImagePicker.with(this)
-				.listener { error, bitmap ->
-					if (error != null) {
-						Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
-					} else {
-						image.setImageBitmap(bitmap)
-					}
-				}
-				.pick(EasyImagePicker.ImageSource.GALLERY)
-	}
+    }
 
-	fun onClickAny(v: View) {
-		EasyImagePicker.with(this)
-				.listener { error, bitmap ->
-					if (error != null) {
-						Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
-					} else {
-						image.setImageBitmap(bitmap)
-					}
-				}
-				.pick(EasyImagePicker.ImageSource.ANY)
-	}
+    fun onClickGallery(v: View) {
+        CropImageDialog.show(this, BitmapFactory.decodeResource(resources, R.drawable.credit_card))
+        return
+
+        EasyImagePicker.with(this)
+            .listener { error, bitmap ->
+                if (error != null) {
+                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    image.setImageBitmap(bitmap)
+                }
+            }
+            .pick(EasyImagePicker.ImageSource.GALLERY)
+    }
+
+    fun onClickAny(v: View) {
+        CropImageDialog.show(this, BitmapFactory.decodeResource(resources, R.drawable.credit_card))
+        return
+
+        EasyImagePicker.with(this)
+            .listener { error, bitmap ->
+                if (error != null) {
+                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    image.setImageBitmap(bitmap)
+                }
+            }
+            .pick(EasyImagePicker.ImageSource.ANY)
+    }
 
 }
---
+++ lib/src/main/java/quevedo/soares/leandro/lib/BaseDialog.kt
@@ -0,0 +1,100 @@
+package quevedo.soares.leandro.lib
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.DialogFragment
+import android.view.*
+
+/**
+ * @author Leandro Soares Quevedo
+ * @author leandro.soares@operacao.rcadigital.com.br
+ * @since 2019-07-01
+ */
+abstract class BaseDialog : DialogFragment() {
+
+    private lateinit var rootView: View
+    private var dismissed: Boolean = false
+
+    //<editor-fold defaultstate="Collapsed" desc="Abstract event handlers">
+
+    /**
+     * Initializes variables and components
+     */
+    protected abstract fun onInitValues()
+
+    /**
+     * Destroys the fragment
+     */
+    protected abstract fun onDispose()
+
+    /**
+     * Loads services and resources
+     */
+    protected abstract fun onPreload(inflater: LayoutInflater, parent: ViewGroup?) : View
+
+    //</editor-fold>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        this.rootView = this.onPreload(inflater, container)
+
+        Handler ().post(this::onInitValues)
+
+        return this.rootView
+    }
+
+    @Suppress("LiftReturnOrAssignment")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = object : Dialog(context, theme) {
+            override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+                if (dismissed) return false
+                else return super.dispatchTouchEvent(ev)
+            }
+        }
+
+        this.initializeDialog(dialog, initialized = false)
+
+        return dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        this.initializeDialog(dialog, true)
+
+        rootView.measure (0, 0)
+    }
+
+    private fun initializeDialog(dialog: Dialog, initialized: Boolean) {
+        dialog.setCancelable(true)
+        dialog.setCanceledOnTouchOutside(false)
+
+        dialog.window?.let {
+            val layoutParams = it.attributes
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+            layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+            layoutParams.gravity = Gravity.CENTER
+            layoutParams.dimAmount = 0f
+
+            it.attributes = layoutParams
+
+            it.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        }
+
+        dialog.setOnDismissListener {
+            dismiss()
+        }
+    }
+
+    override fun dismiss() {
+        this.dismissed = true
+
+        this.onDispose()
+
+        super.dismiss()
+    }
+
+}
